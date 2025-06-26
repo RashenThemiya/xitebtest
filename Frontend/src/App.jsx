406ebabecc84cc27331c6c995f7cc17e52f5d38f
rashen <rashenrashen4@gmail.com>
2025-06-26 12:24:13 +0530

    fix

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,19 @@
 import 'font-awesome/css/font-awesome.min.css';
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
 import PrivateRoute from "./components/PrivateRoute";
 import { AuthProvider } from "./context/AuthContext";
+
 import AdminDashboard from "./pages/AdminDashboard";
-
-import Home from "./pages/Home";
-
 import AllOrdersAdmin from './pages/adminordermangment/adminallorder';
 import AdminOrderDetails from "./pages/adminordermangment/AdminOrderDetails";
 import AdminQuotationPage from "./pages/adminordermangment/AdminQuotationPage";
 import OrderManagementAdmin from './pages/adminordermangment/OrderManagementAdmin';
-import Login from "./pages/Login";
-
 import DrugsPage from './pages/DrugsPage';
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Register from './pages/Register';
 import UserDashboard from './pages/UserDashboard';
 import AddPrescription from './pages/userorderManagement/AddPrecpection';
@@ -24,40 +23,87 @@ import ViewOrders from './pages/userorderManagement/Vieworder';
 
 function App() {
   return (
-        <BrowserRouter basename="/xitebtest">
-    <AuthProvider>
-      
-      <Router>
+    <BrowserRouter basename="/xitebtest">
+      <AuthProvider>
         <div className="min-h-screen bg-gray-100">
           <Routes>
             {/* Public routes */}
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/Register" element={<Register />} />
-           <Route path="/prescriptions/:id/summary" element={<PrescriptionSummary />} />
-            <Route path="/admin-all-orders" element={<AllOrdersAdmin />} />
-            <Route path="/order-management-admin" element={<OrderManagementAdmin />} />
-            <Route path="/admin/orders/:id" element={<AdminOrderDetails />} />
-<Route path="/admin/orders/:id" element={<AdminOrderDetails />} />
-            <Route path="/drugs" element={<DrugsPage />} />
-            {/* Protected routes */}
+            <Route path="/register" element={<Register />} />
+
+            {/* Private routes */}
+            <Route
+              path="/prescriptions/:id/summary"
+              element={
+                <PrivateRoute><PrescriptionSummary /></PrivateRoute>
+              }
+            />
+            <Route
+              path="/admin-all-orders"
+              element={
+                <PrivateRoute><AllOrdersAdmin /></PrivateRoute>
+              }
+            />
+            <Route
+              path="/order-management-admin"
+              element={
+                <PrivateRoute><OrderManagementAdmin /></PrivateRoute>
+              }
+            />
+            <Route
+              path="/admin/orders/:id"
+              element={
+                <PrivateRoute><AdminOrderDetails /></PrivateRoute>
+              }
+            />
+            <Route
+              path="/drugs"
+              element={
+                <PrivateRoute><DrugsPage /></PrivateRoute>
+              }
+            />
             <Route
               path="/admin-dashboard"
-              element={<PrivateRoute><AdminDashboard /></PrivateRoute>}
+              element={
+                <PrivateRoute><AdminDashboard /></PrivateRoute>
+              }
             />
-        <Route path="/admin/prescriptions/:id/quotation" element={<AdminQuotationPage />} />            <Route path="/user-dashboard" element={<PrivateRoute><UserDashboard /></PrivateRoute>} />
-           
-            <Route path="/order-management" element={<PrivateRoute><OrderManagement /></PrivateRoute>} />
-            <Route path="/orders-now" element={<PrivateRoute>< AddPrescription /></PrivateRoute>} />
-           <Route path="/view-orders" element={<PrivateRoute><ViewOrders /></PrivateRoute>} />
-
-
+            <Route
+              path="/admin/prescriptions/:id/quotation"
+              element={
+                <PrivateRoute><AdminQuotationPage /></PrivateRoute>
+              }
+            />
+            <Route
+              path="/user-dashboard"
+              element={
+                <PrivateRoute><UserDashboard /></PrivateRoute>
+              }
+            />
+            <Route
+              path="/order-management"
+              element={
+                <PrivateRoute><OrderManagement /></PrivateRoute>
+              }
+            />
+            <Route
+              path="/orders-now"
+              element={
+                <PrivateRoute><AddPrescription /></PrivateRoute>
+              }
+            />
+            <Route
+              path="/view-orders"
+              element={
+                <PrivateRoute><ViewOrders /></PrivateRoute>
+              }
+            />
           </Routes>
           <ToastContainer />
         </div>
-      </Router>
-    </AuthProvider>
-        </BrowserRouter>
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
 
